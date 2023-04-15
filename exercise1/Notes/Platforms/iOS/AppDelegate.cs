1f872e34a118c220034af124e5426e5d05966d88
Phil Dusseldorf <p.pick@kladusch.de>
2023-04-15 14:00:43 +0200

    :sparkles: added XAML with platform behaviour

--- exercise1/Notes/Platforms/iOS/AppDelegate.cs
+++ exercise1/Notes/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace Notes;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
